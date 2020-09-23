ab12e57bede78e803380f1cb2856400905c5ebea
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-09-23 11:48:34 +0100

    Remove explicit routes for /time and /swapi
    This is to see whether or not explicit routes in server.js are needed for Next.js to serve the dynamic pages correctly

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ const pathMatch = require('path-match')
 const server = express()
 const route = pathMatch()
 server.use('/_next', express.static(path.join(__dirname, '.next')))
-server.get('/time', (req, res) => app.render(req, res, '/time'))
-server.get('/swapi/:id', (req, res) => {
-    const params = route('/swapi/:id')(parse(req.url).pathname)
-    return app.render(req, res, `/swapi/${req.params.id}`, params)
-})
 server.get('*', (req, res) => handle(req, res))
 
 module.exports = server
