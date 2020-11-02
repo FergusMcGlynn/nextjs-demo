5b731b59aaa0d50c76ac59f930edc619d2d821f5
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-11-02 12:44:19 +0000

    Add CloudFront distribution in front of application's API

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ const pathMatch = require('path-match')
 
 const server = express()
 const route = pathMatch()
-server.use('/_next', express.static(path.join(__dirname, '.next')))
+server.use('/_next', express.static(path.join(__dirname, '.next'), { maxAge: "1d" })) // cache static assets for 1 day
 server.get('/time', (req, res) => app.render(req, res, '/time'))
 server.get('/swapi/:id', (req, res) => {
     const params = route('/swapi/:id')(parse(req.url).pathname)
     return app.render(req, res, `/swapi/${req.params.id}`, params)
 })
-server.get('*', (req, res) => handle(req, res))
+server.get('*', (req, res) => {
+    res.setHeader("Cache-Control", "public, max-age=86400") // Cache dynamic pages for a day - you can verify that
+    handle(req, res)                                        // these headers are set using Chrome dev tools 'Network' tab
+})
 
 module.exports = server
